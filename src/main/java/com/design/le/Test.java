d73905e6355e4e9a7637114cedaa224eb9291b14
wanghaidong01 <wanghaidong01@meicai.cn>
2020-07-02 20:23:47 +0800

    add ignore

---
+++ src/main/java/com/design/le/Test.java
@@ -0,0 +1,12 @@
+package com.design.le;
+
+/**
+ * @author Caffe
+ * @date 2020/7/2
+ * @description: TODO
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("1234");
+    }
+}
